d91b515f889ca1c6f8f6291e25ed9034cae72aaf
Pigeon <paxmission@gmail.com>
2016-02-20 12:57:39 +0700

    include m3u8 analyzer

--- checklink.py
+++ checklink.py
@@ -1,11 +1,27 @@
 #Checklink from URL
 import urllib2
-link = 'http://52.76.39.201/m3u8.txt'
+import datetime
+
+import m3u8
+from bitreader import BitReader
+from ts_segment import TSSegmentParser
+
+num_segments_to_analyze_per_playlist = 1
+max_frames_to_show = 30
+
+listurl = 'http://devtab.com/services/thaitv_live/'
+for link in urllib2.urlopen(listurl):
+    link = link.replace('<br/>','\n')
+    result=open("m3u8.txt","w")
+    result.write(link)
+m3u8 = open('m3u8.txt','r')
 i = 0
 j = 0
-for bR in urllib2.urlopen(link):
+for bR in m3u8:
     print "-"*50
     j=j+1
+    datepass = ('{:%Y-%b-%d %H:%M:%S} >>>Pass>>> '.format(datetime.datetime.now()))
+    dateerror = ('{:%Y-%b-%d %H:%M:%S} >>>Error>>> '.format(datetime.datetime.now()))
     try:
         if "http://" or "https://" in bR:
             br=bR
@@ -15,11 +31,11 @@ for bR in urllib2.urlopen(link):
         if urllib2.urlopen(BroTher).getcode() == 200 :
             i=i+1
             print "%d/%d|Ok| %s"%(i,j,br)
-            result=open("NormalLink.txt","a")
-            result.write(br)
+            result=open("normallink.txt","a")
+            result.write(datepass+br)
         else:
             print "%d/%d|No| %s"%(i,j,br)
     except:
         print "%d/%d|Error| %s"%(i,j,br)
-        result=open("urlerror.txt","a")
-        result.write(br)
+        result=open("errorlink.txt","a")
+        result.write(dateerror+br)
