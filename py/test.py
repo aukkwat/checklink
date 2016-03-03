32df2f06a50fdcb9a9b44a73c9008c21db8e575d
Pigeon <paxmission@gmail.com>
2016-03-03 11:28:02 +0700

    last update

---
+++ py/test.py
@@ -0,0 +1,6 @@
+import sys
+print str(sys.argv[0]),str(sys.argv[1])
+print len(sys.argv[1])
+print len(sys.argv[2])
+print len(sys.argv[3])
+print "The Argument are:", str(1+1)
---
+++ py/test2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+import sys
+
+print sys.argv[0]
+print "Number of argument:", len('1+1')
+print "The Argument are:", str(1+1)
